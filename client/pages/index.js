1862844f8b75791b21331143c377f34e918bc6e8
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2023-08-04 03:28:38 +0100

    239. Pass through the cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,15 @@
-import axios from "axios";
+import BuildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>LandingPage</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === "undefined") {
-    // we are on the server!
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: {
-          Host: 'ticket.dev'
-        },
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = BuildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    // we are in the browser!
-    // request can be made with base url of ''
-    const res = await axios.get("/api/users/currentuser");
-    return res.data;
-  }
-
-  return {};
+  return data;
 };
 
 export default LandingPage;
