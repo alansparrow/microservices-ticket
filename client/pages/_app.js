800a107c05d9f2410d38794e05b83e1bcdd396bd
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2023-08-04 06:52:56 +0100

    Send through pageProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,27 +4,45 @@ import "bootstrap/dist/css/bootstrap.css";
 import BuildClient from "../api/build-client";
 import Header from "../components/header";
 
-const AppComponent = ({ Component, pageProps, currentUser }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return (
     <div>
-      <Header currentUser={currentUser} />
+      <Header currentUser={pageProps.currentUser} />
       <Component {...pageProps} />
     </div>
   );
 };
 
 AppComponent.getInitialProps = async (appContext) => {
-  const client = BuildClient(appContext.ctx);
-  const { data } = await client.get("/api/users/currentuser");
+//   const client = BuildClient(appContext.ctx);
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
     pageProps = await appContext.Component.getInitialProps(appContext.ctx);
   }
 
+//   console.log(pageProps);
+
+  /*
+{ pageProps: { currentUser: null }, currentUser: null }
+{
+   pageProps: {
+     currentUser: {
+       id: '64cc633fc764cc124f56c122',
+       email: 'aaa@aaa.com',
+       iat: 1691127575
+     }
+   },
+   currentUser: {
+     id: '64cc633fc764cc124f56c122',
+     email: 'aaa@aaa.com',
+     iat: 1691127575
+   }
+}
+  */
+
   return {
-    pageProps,
-    ...data,
+    pageProps
   };
 };
 
