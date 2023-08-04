686f69197d11c321f1b6292c178ed08f6744b514
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2023-08-04 05:55:20 +0100

    252. Impl Signout

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,31 @@
 // If we even want to include some global CSS into our project,
 // we can only import global CSS into this app file.
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import BuildClient from "../api/build-client";
+import Header from "../components/header";
 
-const MyApp = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default MyApp;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = BuildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
