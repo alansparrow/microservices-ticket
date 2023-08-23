ac9ad62223798ac7685cd1f91654f2d28a60e7fe
Trung Tran <trannguyenbaotrung@gmail.com>
2023-08-23 23:24:01 +0100

    440. Test Job processing

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCreatedEvent } from "@fuzzyrock/common";
 import { Subjects } from "@fuzzyrock/common/build/events/subjects";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -9,6 +10,10 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    throw new Error("Method not implemented.");
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
   }
 }
